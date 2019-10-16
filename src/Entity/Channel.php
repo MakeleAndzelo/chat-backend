e2573d893cafea99d05b5336b1e720440e67ae49
Michał Piotrowski <97.piotrowski.michal@gmail.com>
2019-10-16 22:29:15 +0200

    Add channel entity with the doctrine and api mappings

---
+++ src/Entity/Channel.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+class Channel
+{
+    private $id;
+
+    private $name;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+}
